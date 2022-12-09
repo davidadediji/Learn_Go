3ba07fbc3a59f76174a87938d8b0b0ee80479316
davidadediji <davidadediji@gmail.com>
2022-12-09 02:36:52 +0100

    fix:prettier

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,23 +1,21 @@
 package main
+
 import "fmt"
 
+func main() {
 
-func main(){
-	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
 
 	//print out the types of a variable and the constants
 	// fmt.Printf("conferenceTickets is %T\n", remainingTickets)
+	greetUsers()
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v, are still avaliable.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-
 	//Arrays - arrays in Go have fixed size
 	var bookings = []string{}
-	
 
 	var firstName string
 	var lastName string
@@ -38,15 +36,19 @@ func main(){
 
 	remainingTickets = remainingTickets - userTickets
 	// bookings[0] = firstName + " " + lastName
-	bookings = append(bookings, firstName + " "  + lastName)
+	bookings = append(bookings, firstName+" "+lastName)
 
 	// fmt.Printf("The whole slice: %v\n", bookings)
 	// fmt.Printf("The first value: %v\n", bookings[0])
 	// fmt.Printf("Slice type: %T\n", bookings)
 	// fmt.Printf("Slice length %v\n", len(bookings))
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmaton email at %v\n", firstName,lastName,userTickets,email)
-	fmt.Printf("remains %v tickets\n",remainingTickets)
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmaton email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("remains %v tickets\n", remainingTickets)
 	fmt.Printf("All the bookings %v\n", bookings)
 }
 
+func greetUsers() {
+	conferenceName := "Go Conference"
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+}
