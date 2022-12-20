9d1084484f3afd5fe8b7ac1a0a1cdebb769c0e13
davidadediji <davidadediji@gmail.com>
2022-12-20 23:55:36 +0100

    refactor:encapsulate  functions

--- booking-app/README.md
+++ booking-app/README.md
@@ -4,12 +4,28 @@
 //arrays does not allow mixed data types
 //To declare an array in Go
 
+
+
+// bookingsArray := [34]string{} //array
+	// bookingsArray[0] = "5"
+	// f.Println(bookingsArray)
+
+	//print out the types of a variable and the constants
+	// fmt.Printf("conferenceTickets is %T\n", remainingTickets)
+
 var paidBooks [50]string
 f.Printf("The lenght of the array is:%v", len(paidBooks))
 
 books := []string{} // -> slice of strings a list (array ) that is dynamic in size. A slice is an array abstration with dynamic size.
 books = append(books, "david", "favour")
 f.Println(books)
+
+
+    // f.Printf("The whole slice: %v\n", bookings)
+    // f.Printf("The first value: %v\n", bookings[0])
+    // f.Printf("Slice type: %T\n", bookings)
+    // f.Printf("Slice length %v\n", len(bookings))
+    // f.Print("This is David")
 
 length of array in Go
 
--- booking-app/main.go
+++ booking-app/main.go
@@ -3,65 +3,27 @@ package main
 import (
 	f "fmt"
 	"strings"
-	"time"
 )
 
 func main() {
+	conferenceName := "Go conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
-	bookings := []string{}        //slice
-	bookingsArray := [34]string{} //array
-	bookingsArray[0] = "5"
-	f.Println(bookingsArray)
-
-	//print out the types of a variable and the constants
-	// fmt.Printf("conferenceTickets is %T\n", remainingTickets)
-	greetUsers()
-	f.Println(time.Now().UnixNano())
-	f.Printf("We have total of %v tickets and %v, are still avaliable.\n", conferenceTickets, remainingTickets)
-	f.Println("Get your tickets here to attend")
+	bookings := []string{} //slice
+	greetUsers(conferenceName, conferenceTickets, remainingTickets)
 
 	//infinite loop
 	for {
-		//declare variables and leave unassigned....
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-		// ask user for their name
-		f.Println("Enter your first name: ")
-		f.Scan(&firstName)
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidEmail, isValidName, isValidTicketNumber  := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
-		f.Println("Enter your last name: ")
-		f.Scan(&lastName)
-
-		f.Println("Enter your email address: ")
-		f.Scan(&email)
-
-		f.Println("Enter number of tickets: ")
-		f.Scan(&userTickets)
-
-		if userTickets <= remainingTickets {
+		if isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
 			bookings = append(bookings, firstName+" "+lastName)
 
-			// f.Printf("The whole slice: %v\n", bookings)
-			// f.Printf("The first value: %v\n", bookings[0])
-			// f.Printf("Slice type: %T\n", bookings)
-			// f.Printf("Slice length %v\n", len(bookings))
-			f.Print("This is David")
-
 			f.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmaton email at %v\n", firstName, lastName, userTickets, email)
 			f.Printf("remains %v tickets\n", remainingTickets)
-
-			firstNames := []string{}
-
-			//for each
-			for _, booking := range bookings {
-				names := strings.Fields(booking)
-
-				firstNames = append(firstNames, names[0])
-			}
+			firstNames := getFirstNames(bookings)
 			f.Printf("The first names of bookings are: %v\n", firstNames)
 
 			//conditionals (if-else)
@@ -72,14 +34,67 @@ func main() {
 				break
 			}
 		} else {
-			f.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
-			continue
+			if !isValidName {
+				f.Println("first name or last name you entered is too short")
+			}
+			if !isValidEmail {
+				f.Println("email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				f.Println("number of tickets you entered is invalid")
+			}
+
 		}
 	}
 
 }
 
-func greetUsers() {
-	conferenceName := "Go Conference"
-	f.Printf("Welcome to %v booking application\n", conferenceName)
+func greetUsers(confName string, confTickets int, remainingTickets uint) {
+	f.Printf("Welcome to %v booking application\n", confName)
+	// f.Println(time.Now().UnixNano())
+	f.Printf("We have total of %v tickets and %v, are still avaliable.\n", confTickets, remainingTickets)
+	f.Println("Get your tickets here to attend")
+}
+
+func getUserInput() (string, string, string, uint) {
+	//declare variables and leave unassigned....
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask user for their name
+	f.Println("Enter your first name: ")
+	f.Scan(&firstName)
+
+	f.Println("Enter your last name: ")
+	f.Scan(&lastName)
+
+	f.Println("Enter your email address: ")
+	f.Scan(&email)
+
+	f.Println("Enter number of tickets: ")
+	f.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func getFirstNames(bookings []string) []string {
+	firstNames := []string{}
+	//for each
+	for _, booking := range bookings {
+		names := strings.Fields(booking)
+
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+
+}
+
+func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	//Go can return multiple values 
+	return isValidEmail, isValidName, isValidTicketNumber
 }
